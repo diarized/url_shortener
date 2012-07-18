a189c41dff8477041167ecb1a7d4f8a653325c3d
Artur Kaminski <my.home.my.castle@gmail.com>
2012-07-18 19:25:53 +0000

    twisted renamed. Cannot be named as module.

---
+++ twisted_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from twisted.web.server import Site  # Site is a server factory for HTTP
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+import cache
+
+
+class PrintPostBody(Resource):  # Resources are what Site knows how to deal with
+    isLeaf = True  # Disable child lookup
+
+    def render_POST(self, request):  # Define a handler for POST requests
+        print request.content.read()  # Get the request body from this file-like object
+        return "" # Define the response body as empty
+
+reactor.listenTCP(8888, Site(PrintPostBody()))
+reactor.run()
+
+
