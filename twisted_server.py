ad86ac6454b862393af381d3f166a1cd4d68cc21
Artur Kamiński <artur@stonith.pl>
2012-07-20 00:13:13 +0200

    Anty-abusing works.

--- twisted_server.py
+++ twisted_server.py
@@ -12,6 +12,8 @@ import cgi
 import cache
 import time
 
+ABUSE_INTERVAL = 5
+
 def short_id(num):
     return "".join(sample(digits + ascii_letters, num))
 
@@ -26,20 +28,29 @@ class UrlShortener(Resource):  # Resources are what Site knows how to deal with
 
     def render_GET(self, request):
         if request.path == '/':
-            #request.__class__ = twisted.web.server.Request
             return index_template
         else:
-            short_url = request.path.lstrip('/')
-            return redirectTo(str(cache.tcache.get_value(short_url)), request)
+            url_id = request.path.lstrip('/')
+            return redirectTo(str(cache.tcache.get_value(url_id)), request)
 
     def render_POST(self, request):  # Define a handler for POST requests
-        if cache.tabuse.get_value(request.getClientIP()):
+        client_ip = request.getClientIP()
+        try:
+            last_time = float(cache.tabuse.get_value(client_ip))
+        except:
+            last_time = time.time() - ABUSE_INTERVAL
+            raise
+        delta = time.time() - last_time
+        log.msg('delta = {0}'.format(delta))
+        if delta < ABUSE_INTERVAL:
+            log.msg('Abuser from IP {0} connected at {1}.'.format(client_ip, last_time))
             return index_template
-        cache.tabuse.put(request.getClientIP, time.time())
         full_url = cgi.escape(request.args["full_url"][0])
         url_id = short_id(5)
         log.msg("URL {0} ===> {1}".format(full_url, url_id))
         cache.tcache.put(url_id, full_url)
+        cache.tabuse.put(client_ip, float(time.time()))
+        log.msg('Not abuser from IP {0}'.format(client_ip))
         return shurl_template.format(url_id)
 
 log.startLogging(sys.stdout)
