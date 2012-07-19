a0d66332dac70664c16ec6a67ec4d4b4531f20ce
Artur Kamiński <artur@stonith.pl>
2012-07-20 00:14:06 +0200

    DBM for abusers.

--- cache.py
+++ cache.py
@@ -2,10 +2,12 @@ import os
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
+dot = os.path.dirname(__file__)
+
 cache_opts = {
     'cache.type': 'file',
-    'cache.data_dir': os.path.join(os.path.dirname(__file__),'url_shortener_data'),
-    'cache.lock_dir': os.path.join(os.path.dirname(__file__),'url_shortener_lock'),
+    'cache.data_dir': os.path.join(dot,'url_shortener_data'),
+    'cache.lock_dir': os.path.join(dot,'url_shortener_lock'),
 }
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
@@ -14,10 +16,10 @@ tcache = cache.get_cache("stuff", type='dbm') #, expire=3600)
 
 abuse_opts = {
     'cache.type': 'file',
-    'cache.data_dir': os.path.join(os.path.dirname(__file__),'abuser_data'),
-    'cache.lock_dir': os.path.join(os.path.dirname(__file__),'abuser_lock'),
+    'cache.data_dir': os.path.join(dot,'abuser_data'),
+    'cache.lock_dir': os.path.join(dot,'abuser_lock'),
 }
 
 abuse = CacheManager(**parse_cache_config_options(abuse_opts))
-tabuse = cache.get_cache("abuse", type='dbm' , expire=5)
+tabuse = abuse.get_cache("stuff", type='dbm') #, expire=5)
 
